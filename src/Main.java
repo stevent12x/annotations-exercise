1bcf20e1e9335b3fdad0105dc3c5a54b44126f57
Steven Turner <steven.turner12x@gmail.com>
2019-08-06 11:20:36 -0500

    fixed my own problems

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,8 @@
 public class Main {
     @SuppressWarnings("deprecation")
+
     public static void main(String[] args) {
+
         Author author = new Author("Sandi", "Metz");
 
         author.addBook("Practical Object-Oriented Design in Ruby");
--- src/Person.java
+++ src/Person.java
@@ -2,6 +2,7 @@
  * This source file is subject to the license that is bundled with this package in the file LICENSE.
  */
 public class Person {
+
     protected String firstName;
     protected String lastName;
 
